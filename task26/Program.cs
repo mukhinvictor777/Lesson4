221afb23e27225933f1a686c0fc435ce9ffada28
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-11 23:18:14 +0600

    task26 нашли количество цифр в числе

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿int getNumberFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0") Console.WriteLine($"Введите целое число, вы вввели {userLine}"); else break;
+    }
+    return result;
+}
+
+void printInConsoleWithColor(string information, ConsoleColor color)
+{
+     Console.ForegroundColor = color;
+     Console.WriteLine(information);
+     Console.ResetColor();
+}
+
+int getCountOfDigit(int userNumber)
+{
+    int count = 1;
+    userNumber = Math.Abs(userNumber);
+    while (userNumber / 10 > 0)
+    {
+        userNumber = userNumber / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine();
+printInConsoleWithColor("Введите целое число:", ConsoleColor.DarkMagenta);
+int userNumber = getNumberFromUser("");
+int countOfDigit = getCountOfDigit(userNumber);
+Console.WriteLine($"Количество цифр в числе {userNumber} = {countOfDigit}");
+Console.WriteLine();
