c3571021992051620c939c07058cc3f0ca738beb
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-11 12:38:59 +0600

    решили Фибоначи

--- Lecture3fibonachi/Program.cs
+++ Lecture3fibonachi/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Fibonacci (int n)
+{
+    if (n == 1 || n ==2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i <= 40; i++)
+{
+  Console.WriteLine($"{i} = {Fibonacci(i)}");
+  // Console.WriteLine(Fibonacci(i));
+}
