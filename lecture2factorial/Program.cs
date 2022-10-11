e3578ce861777cf5ab2d875c93fb6d8bab619526
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-11 11:12:41 +0600

    Решаем факториал

---
+++ lecture2factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+
+FillImage(6, 6);
