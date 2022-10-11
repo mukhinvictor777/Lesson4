dc3d937495885d95a70f617b7ccac856e89fe1c2
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-11 22:55:10 +0600

    task24 нашли сумму чисел

--- task24/Program.cs
+++ task24/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getNumberFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0") Console.WriteLine($"Введите целое число, вы вввели {userLine}"); else break;
+    }
+    return result;
+}
+
+void printInConsoleWithColor(string information, ConsoleColor color)
+{
+     Console.ForegroundColor = color;
+     Console.WriteLine(information);
+     Console.ResetColor();
+}
+
+int getSumOfRange(int startPoint, int endPoint)
+{
+    int result = 0;
+    if (startPoint > endPoint)
+    {
+        printInConsoleWithColor($"Верхняя граница отрицательна {endPoint}", ConsoleColor.DarkYellow);
+        int buffer = startPoint;
+        startPoint = endPoint;
+        endPoint = buffer;
+    }
+    for (int i = startPoint; i <= endPoint; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+Console.WriteLine();
+printInConsoleWithColor($"Введите верхную границу для суммы ряда", ConsoleColor.DarkMagenta);
+int userNumber = getNumberFromUser("");
+int sumOfRange = getSumOfRange(1, userNumber);
+Console.WriteLine($"Сумма чисел от 1 до {userNumber} = {sumOfRange}");
+Console.WriteLine();
